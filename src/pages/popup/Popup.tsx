b13bb313291e4bd0ebac3959b51eeac2be65effb
echonabin <echonabin@gmail.com>
2022-12-22 13:33:33 +0545

    basic tailwind added

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,15 @@
 import React from "react";
-import logo from "@assets/img/logo.svg";
 import "@pages/popup/Popup.css";
 
 const Popup = () => {
+  const openOptionHandler = () => {
+    chrome.runtime.openOptionsPage();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div className="bg-teal-200 flex flex-col items-center w-full h-screen py-3 px-3 shadow">
+      <h1>Welcome to Rs kit.</h1>
+      <img src="device_128" alt="" />
+      <div>welcome aboard</div>
     </div>
   );
 };
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/pages/**/*.{ts,tsx,js,jsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
